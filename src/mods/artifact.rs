b8bd36254726087e59c5b851406a8dc8d3f2f214
Gegy <gegy1000@gmail.com>
2021-01-02 09:34:59 +0200

    capture SNAPSHOT as apart of version instead of classifier

--- src/mods/artifact.rs
+++ src/mods/artifact.rs
@@ -13,7 +13,7 @@ pub fn parse(path: impl AsRef<Path>) -> Option<ArtifactId> {
 
     let name = path.file_name()?.to_str()?;
 
-    let id_pattern = Regex::new(r#"(.+?)-([\d.]+)(?:\+.+?(?=(?:\.jar)|-))?-?(.*?)\.jar"#).unwrap();
+    let id_pattern = Regex::new(r#"(.+?)-([\d.]+(?:-SNAPSHOT)?)(?:\+.+?(?=(?:\.jar)|-))?-?(.*?)\.jar"#).unwrap();
     let id_captures = id_pattern.captures(name).unwrap()?;
 
     let name = id_captures.get(1)?.as_str().to_owned();
